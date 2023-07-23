a269bc990dd459c53271227d1331960db36234e7
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-07-23 14:35:42 +0530

    remove empty file

--- apps/cubik-ui/src/app/layout.tsx
+++ apps/cubik-ui/src/app/layout.tsx
@@ -5,6 +5,7 @@ import dynamic from 'next/dynamic';
 
 import { Container } from '../utils/chakra';
 import theme from '~/config/chakra.config';
+import Head from 'next/head';
 
 const WalletContext: any = dynamic(() => import('../context/wallet-context'), {
   ssr: false,
@@ -17,6 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <WalletContext>
           <ChakraProvider theme={theme}>
