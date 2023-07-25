3d6debb3ffb06c2bc1390c534c75a0542587b7a8
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-07-26 00:45:06 +0530

    project explore to static path

--- apps/cubik-ui/src/pages/admin/index.tsx
+++ apps/cubik-ui/src/pages/admin/index.tsx
@@ -1,37 +1,25 @@
 import { Container } from '@chakra-ui/layout';
-import { GetStaticProps } from 'next';
+import React from 'react';
+import SEO from '~/components/SEO';
 import AdminControls from '~/components/pages/projects/admin/AdminControls';
-import ProjectsExplorer from '~/components/pages/projects/project-explorer/ProjectsExplorer';
 
-type projectsPropsType = {
-  allProjectsData: {
-    data: any;
-  };
-};
-
-const Projects = (_props: projectsPropsType) => {
+const Admin = () => {
   return (
     <>
-      {/* <SEO
+      <SEO
         title={`Projects - Cubik`}
         description={`Browse projects and Cubik and support them`}
         image={`https://res.cloudinary.com/demonicirfan/image/upload/v1684179451/cubik%20og.png`}
-      /> */}
+      />
       <Container
         px={{ base: '0.6rem', sm: '0.8rem', md: '2rem', xl: '0px' }}
         maxW="7xl"
         py={{ base: '24px', md: '40px' }}
       >
         <AdminControls />
-        <ProjectsExplorer />
       </Container>
     </>
   );
 };
-export const getStaticProps: GetStaticProps = async () => {
-  return {
-    props: {},
-    revalidate: 1 * 60 * 60,
-  };
-}; 
-export default Projects;
+
+export default Admin;
