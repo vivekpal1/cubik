d46662807322d1b80f1bb68d0cec4d147841c041
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-08-04 14:51:46 +0000

    test

--- apps/cubik-web/src/app/api/ip/route.ts
+++ apps/cubik-web/src/app/api/ip/route.ts
@@ -5,7 +5,11 @@ import requestIp from "request-ip";
 export async function GET(req: NextRequest) {
   const ip = requestIp.getClientIp(req);
 
+  const forwarded = req.headers.get("x-forwarded-for");
+
   return NextResponse.json({
-    ip,
+    requestIp: ip,
+    vercel: req.ip,
+    geo: req.geo,
   });
 }
