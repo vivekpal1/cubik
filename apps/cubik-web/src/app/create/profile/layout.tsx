2228aab7524eadcf09ed0af20ad1e0ee8caa4a98
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-08-05 16:28:23 +0530

    wip: create project

--- apps/cubik-web/src/app/create/profile/layout.tsx
+++ apps/cubik-web/src/app/create/profile/layout.tsx
@@ -1,15 +1,14 @@
-import { Box, Container } from "@/utils/chakra";
+"use client";
+import { Box } from "@/utils/chakra";
 import React from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 interface Props {
   children: React.JSX.Element | React.JSX.Element[];
 }
 const ProfileLayout = ({ children }: Props) => {
-  return (
-    <>
-      <Box mt={10}>{children}</Box>
-    </>
-  );
+  const { publicKey } = useWallet();
+  return <>{publicKey ? <Box mt={10}>{children}</Box> : <>Connect Wallet</>}</>; // update to wallet connet button
 };
 
 export default ProfileLayout;
