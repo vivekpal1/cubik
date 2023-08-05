77ac8f67c5841e7c1f29b283ba1889df204c9622
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-08-06 01:43:07 +0530

    fix: build

--- apps/cubik-web/next.config.mjs
+++ apps/cubik-web/next.config.mjs
@@ -1,7 +1,21 @@
 import "./src/env.mjs";
+// @ts-ignore
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 /** @type {import("next").NextConfig} */
 const config = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
+  transpilePackages: ["@cubik/api", "@cubik/database"],
   experimental: { serverActions: true, appDir: true },
   images: {
     domains: [
