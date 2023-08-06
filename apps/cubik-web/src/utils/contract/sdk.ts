4c603fb3599bd90044dddd592540d17a2e2b5534
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-08-06 19:56:47 +0530

    fix: contracts

---
+++ apps/cubik-web/src/utils/contract/sdk.ts
@@ -0,0 +1,28 @@
+"use client";
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import { SDK } from "cubik-main";
+import { web3 } from "@coral-xyz/anchor";
+import { env } from "@/env.mjs";
+
+const RPC_URL =
+  env.NEXT_PUBLIC_SOLANA_NETWORK === "mainnet-beta"
+    ? env.NEXT_PUBLIC_RPC_MAINNET_URL
+    : env.NEXT_PUBLIC_RPC_DEVNET_URL;
+
+export const connection = new web3.Connection(RPC_URL, "confirmed");
+
+export const getSdk = (
+  wallet: NodeWallet,
+  customConnection: web3.Connection = connection
+) => {
+  const sdk = new SDK(
+    wallet,
+    customConnection,
+    {
+      commitment: "confirmed",
+    },
+    "mainnet-beta"
+  );
+
+  return sdk;
+};
