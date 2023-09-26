57da179019df5549edfa8f636d7a53f3f73945a9
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-09-26 22:00:08 +0530

    wip

--- apps/cubik-web/src/app/components/common/wallet/index.tsx
+++ apps/cubik-web/src/app/components/common/wallet/index.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { useClipboard, Center, HStack, Text } from "@/utils/chakra";
+import { useClipboard, Center, HStack, Text, useToast } from "@/utils/chakra";
 import type { FC, SetStateAction } from "react";
 import { useEffect } from "react";
+import { SuccessToast } from "../../toasts/Toasts";
 //import { TbCopy } from "react-icons/tb";
-// import { SuccessToast } from "../toasts/Toasts";
 
 type PropsType = {
   size?: string;
@@ -22,7 +22,7 @@ export const WalletAddress: FC<PropsType> = ({
 }: PropsType) => {
   const { onCopy, value, setValue, hasCopied } = useClipboard("");
   const addr = walletAddress;
-
+  const toast = useToast();
   useEffect(() => {
     if (addr) {
       setValue(addr as SetStateAction<string>);
@@ -116,30 +116,43 @@ export const WalletAddress: FC<PropsType> = ({
             </svg>
           )}
         </Center>
-        <Text fontSize={textSize} color={fillColor} fontWeight="600">
-          {truncatedAddr}
-        </Text>
+        <HStack>
+          <Text fontSize={textSize} color={fillColor} fontWeight="600">
+            {truncatedAddr}
+          </Text>
+          <Center
+            onClick={() => {
+              onCopy(); // todo: this is not working
+              SuccessToast({ toast, message: "Wallet Address Copied" });
+            }}
+            transform={"scale(0.95)"}
+            opacity={"0.8"}
+            transition={"all 0.2s"}
+            _hover={{
+              transform: "scale(1)",
+              opacity: "1",
+              transition: "all 0.2s",
+            }}
+            as="button"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="#fff"
+              width="20"
+              height="20"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"
+              />
+            </svg>
+          </Center>
+        </HStack>
       </HStack>
-      {copy && (
-        <Center
-          onClick={() => {
-            onCopy(); // todo: this is not working
-            // SuccessToast({ toast, message: "Wallet Address Copied" });
-          }}
-          transform={"scale(0.95)"}
-          opacity={"0.8"}
-          transition={"all 0.2s"}
-          _hover={{
-            //transform: 'scale(1)',
-            opacity: "1",
-            transition: "all 0.2s",
-          }}
-          as="button"
-        >
-          {/*  @todo  */}
-          {/* <TbCopy width={3} height={3} color={fillColor} /> */}
-        </Center>
-      )}
     </HStack>
   );
 };
