4c22ee811dd25b2760d322a5d9720f75bae73a31
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-10-02 10:42:48 +0530

    fix: wallet context

--- apps/cubik-web/src/context/wallet-context.tsx
+++ apps/cubik-web/src/context/wallet-context.tsx
@@ -1,42 +1,20 @@
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import {
-  BackpackWalletAdapter,
-  CoinbaseWalletAdapter,
-  GlowWalletAdapter,
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  SolletExtensionWalletAdapter,
-  TokenaryWalletAdapter,
-  TorusWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
-import { web3 } from '@coral-xyz/anchor';
-import type { FC, ReactNode } from 'react';
-import { useMemo } from 'react';
+import { web3 } from "@coral-xyz/anchor";
+import type { FC } from "react";
+import { useMemo } from "react";
 import { env } from "@/env.mjs";
 
-require('@solana/wallet-adapter-react-ui/styles.css');
+require("@solana/wallet-adapter-react-ui/styles.css");
 
-const WalletContext: FC<{ children: ReactNode }> = ({ children }) => {
+const WalletContext: FC<{ children: JSX.Element }> = ({ children }) => {
   const network =
-    env.NEXT_PUBLIC_SOLANA_NETWORK === 'mainnet-beta'
+    env.NEXT_PUBLIC_SOLANA_NETWORK === "mainnet-beta"
       ? WalletAdapterNetwork.Mainnet
       : WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => web3.clusterApiUrl(network), [network]);
-  const wallets = useMemo(
-    () => [
-      new BackpackWalletAdapter(),
-      new CoinbaseWalletAdapter(),
-      new GlowWalletAdapter(),
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-      new SolletExtensionWalletAdapter(),
-      new TorusWalletAdapter(),
-      new TokenaryWalletAdapter(),
-    ],
-    [],
-  );
+  const wallets = useMemo(() => [], []);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
