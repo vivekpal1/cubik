66ae0b0401ba47d3b283cdcca3c0a2367b9be8d7
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-10-04 22:50:00 +0530

    fix: wip

---
+++ apps/cubik-dashboard/src/app/events/components/Tabs.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Tabs = () => {
+  return <div className="bg-blue-950">Tabs</div>;
+};
---
+++ apps/cubik-dashboard/src/app/events/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { EventHeader } from "./components/EventHeader";
+import { Tabs } from "./components/Tabs";
+
+const EventInfoPage = () => {
+  return (
+    <>
+      <EventHeader />
+      <Tabs />
+    </>
+  );
+};
+export default EventInfoPage;
