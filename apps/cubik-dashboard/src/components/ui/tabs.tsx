854bbc45ad615a991a4d5f20e1c5d5967eaf41b7
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-10-06 20:55:28 +0530

    fix: tabs

--- apps/cubik-dashboard/src/components/ui/tabs.tsx
+++ apps/cubik-dashboard/src/components/ui/tabs.tsx
@@ -29,12 +29,12 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow",
+      "inline-flex items-center justify-center whitespace-nowrap  px-3 py-1 text-base font-medium  transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:border-b border-white  data-[state=active]:text-foreground data-[state=active]:shadow",
       className
     )}
     {...props}
   />
-))
+));
 TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
 
 const TabsContent = React.forwardRef<
