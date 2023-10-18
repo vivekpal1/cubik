e30c04fdd7132a8cc0513ede056afcb8cc654fcd
Arpita Ganata <ganatraarpita@gmail.com>
2023-10-18 22:43:23 +0530

    accordion component (#316)
    * accordion component
    
    * import accordion

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -3,5 +3,6 @@ export * from "./components/ui/text";
 export * from "./components/ui/tabs";
 export * from "./components/ui/table";
 export * from "./components/ui/input";
+export * from "./components/ui/accordion";
 export * from "./icons/icon";
 export * from "./components/ui/helperText";
