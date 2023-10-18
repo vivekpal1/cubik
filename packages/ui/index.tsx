60cad97316625e9299c9b6562d0ba001226be81c
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-10-18 14:20:00 +0530

    fix: input

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -4,3 +4,4 @@ export * from "./components/ui/tabs";
 export * from "./components/ui/table";
 export * from "./components/ui/input";
 export * from "./icons/icon";
+export * from "./components/ui/helperText";
