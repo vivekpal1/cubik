a159b349c073640c4093f770edfaf0862d8a1160
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-10-18 14:20:52 +0530

    fix: input

--- packages/ui/components/ui/helperText.tsx
+++ packages/ui/components/ui/helperText.tsx
@@ -3,7 +3,7 @@ import { cn } from "../../lib/utils";
 import { VariantProps, cva } from "class-variance-authority";
 import { forwardRef } from "react";
 
-const helpertextVariants = cva("transition-colors", {
+const helperTextVariants = cva("transition-colors", {
   variants: {
     variant: {
       success: "text-[#29CC44]",
@@ -24,7 +24,7 @@ const helpertextVariants = cva("transition-colors", {
 });
 
 type TextProps = React.HTMLAttributes<HTMLElement> &
-  VariantProps<typeof helpertextVariants> & {
+  VariantProps<typeof helperTextVariants> & {
     asChild?: boolean;
     as?: "p" | "span" | "div" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   };
@@ -34,11 +34,11 @@ const HelperText = forwardRef<HTMLElement | null, TextProps>(
     const Comp = asChild ? Slot : Tag;
     return (
       // @ts-ignore
-      <Comp className={cn(helpertextVariants({ className }))} {...props} />
+      <Comp className={cn(helperTextVariants({ className }))} {...props} />
     ); // ref is missing here
   }
 );
 
 HelperText.displayName = "HelperText";
 
-export { HelperText, helpertextVariants };
+export { HelperText, helperTextVariants };
