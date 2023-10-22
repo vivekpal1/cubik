a0862b3b64d3afeb33febe81aec1a6920a111cb8
Sayan Biswas <me@sayanbiswas.in>
2023-10-22 19:23:24 +0530

    feat: interactive image-upload component  (#328)
    * fix build errors
    * rewrite input component
    * fix import in accordion (TODO: properly configure tsconfig paths in entire project)
    
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>
    
    * feat: add storybook with tailwind configured
    
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>
    
    * add text-area component
    
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>
    
    * Add storybooks for button and text-area
    
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>
    
    * update pnpm lock
    
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>
    
    * simplify storybook
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * feat: interactive image-upload component
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * create heading component
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    * add modal component
    
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>
    
    ---------
    
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>

--- packages/ui/stories/TextArea.stories.tsx
+++ packages/ui/stories/TextArea.stories.tsx
@@ -1,51 +1,39 @@
-import type { Meta, StoryFn } from "@storybook/react";
 import { Textarea } from "../components/ui/text-area";
 
-const meta = {
+export default {
   title: "TextArea",
   component: Textarea,
   parameters: {
     layout: "centered",
   },
   tags: ["autodocs"],
-} satisfies Meta<typeof Textarea>;
+} 
 
-export default meta;
-
-// type Story = StoryObj<typeof meta>;
-
-const Template: StoryFn<typeof Textarea> = (args) => <Textarea {...args} />;
-export const Default: StoryFn<typeof Textarea> = Template.bind({});
-export const Error: StoryFn<typeof Textarea> = Template.bind({});
-export const Disabled: StoryFn<typeof Textarea> = Template.bind({});
-export const Small: StoryFn<typeof Textarea> = Template.bind({});
-export const NonResizable: StoryFn<typeof Textarea> = Template.bind({});
-
-Default.args = {
+export const Default = {
   size: "md",
   state: "default",
   resizable: true,
 };
 
-Error.args = {
+export const Error = {
   size: "md",
   state: "error",
   resizable: true,
 };
 
-Disabled.args = {
+export const Disabled = {
   size: "md",
   state: "disabled",
   resizable: true,
 };
 
-Small.args = {
+export const Small = {
   size: "sm",
   state: "default",
   resizable: true,
 };
 
-NonResizable.args = {
+export const NonResizable = {
   size: "md",
   state: "default",
   resizable: false,
