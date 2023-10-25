0f923d6e9b23f3c496cac7c18c1597fcda772565
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-10-25 23:44:26 +0530

    feat: added component

--- apps/component-show/src/app/page.tsx
+++ apps/component-show/src/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Link href={"/com/avatar"}>
+        <button className="h-8 bg-violet-500 text-white px-7 rounded">
+          Avatar
+        </button>
+      </Link>
+    </main>
   );
 }
