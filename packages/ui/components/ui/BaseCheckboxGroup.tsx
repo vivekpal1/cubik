88a8eb6acf6e0cec2adbbede4ad631c98d07b6f8
vivek <vivekpal@duck.com>
2023-10-28 22:37:41 +0530

    square docs: Alert (#347)
    * fix: build
    
    * fix: color spell
    
    * feat: Squads V4 migration (#310)
    
    * feat: wip
    
    * fix: lock
    
    * export components
    
    * fix build issues
    
    * component / alert
    
    * icon / infoTriangle
    
    * square-docs / Alert
    
    * fix
    
    * move page to txt
    
    * pageNew
    
    * tsx - txt
    
    ---------
    
    Co-authored-by: dhruvsol <dhruvrajsinghsolanki161@gmail.com>
    Co-authored-by: dhruvsol <89624156+dhruvsol@users.noreply.github.com>

--- packages/ui/components/ui/BaseCheckboxGroup.tsx
+++ packages/ui/components/ui/BaseCheckboxGroup.tsx
@@ -1,5 +1,5 @@
+"use client";
 import React, { useState } from 'react';
-
 import { Checkbox } from './Checkbox';
 import { InputLabel } from './inputLabel';
 
--- packages/ui/components/ui/Toast.tsx
+++ packages/ui/components/ui/Toast.tsx
@@ -1,5 +1,5 @@
+"use client";
 import React, { useEffect, useState } from 'react';
-
 import Alert from './Alert';
 
 interface ToastProps {
