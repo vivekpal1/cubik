e7dcd1b694ba15b9e3c2eb1144b83251bd86b611
vivekpal1 <31vivekpal@gmail.com>
2023-11-06 21:38:36 +0530

    fix: transpilePackages

--- apps/squares-docs/next.config.mjs
+++ apps/squares-docs/next.config.mjs
@@ -6,7 +6,7 @@ import { withAxiom } from "next-axiom";
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@cubik/ui'],
+  transpilePackages: ['@cubik/ui', '@cubik/wallet'],
   reactStrictMode: true,
   images: {
     domains: ['imagedelivery.net', 'pbs.twimg.com'],
